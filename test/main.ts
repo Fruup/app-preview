a91a52f73428d1783ced7e342fe66a2a4a955fe6
Leon Scherer <55341374+Fruup@users.noreply.github.com>
2025-11-08 02:01:06 +0100

    dev

--- test/main.ts
+++ test/main.ts
@@ -2,10 +2,12 @@ console.log("Hello from App Preview! 🦆");
 console.log("Here are all of 🦆's secrets:");
 console.log(Bun.env);
 
-Bun.serve({
+const server = Bun.serve({
   port: Bun.env.PORT,
   hostname: "0.0.0.0",
   fetch(req, server) {
     return new Response("Hello from App Preview! 🦆");
   },
 });
+
+console.log(`Server is running at ${server.url}`);
