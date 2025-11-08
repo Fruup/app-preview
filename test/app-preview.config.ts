2b4405c2a02229605403deffd528f31b1df913e4
Leon Scherer <55341374+Fruup@users.noreply.github.com>
2025-11-08 01:17:19 +0100

    fix test env

--- test/app-preview.config.ts
+++ test/app-preview.config.ts
@@ -1,12 +1,14 @@
-defineConfig(({ OnePasswordEnvGenerator, appNameDomainInfix }) => ({
-  root: "test",
-  dockerComposePath: "docker-compose.yml",
-  envGenerator: OnePasswordEnvGenerator.create(
-    "op://Work/n5lqo3s7ncutk4cnungs5nf27y/env"
-  ),
-  expose: {
-    test: {
-      domain: `${appNameDomainInfix}.traefik.me`,
+export default defineConfig(
+  ({ OnePasswordEnvGenerator, appNameDomainInfix }) => ({
+    root: "test",
+    dockerComposePath: "docker-compose.yml",
+    envGenerator: OnePasswordEnvGenerator.create(
+      "op://Work/n5lqo3s7ncutk4cnungs5nf27y/env"
+    ),
+    expose: {
+      test: {
+        domain: `${appNameDomainInfix}.traefik.me`,
+      },
     },
-  },
-}));
+  })
+);
