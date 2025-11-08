15ff834c5ecebeaf903a8429a95280385566c0bd
Leon Scherer <55341374+Fruup@users.noreply.github.com>
2025-11-08 01:10:14 +0100

    fix test env

--- test/app-preview.config.ts
+++ test/app-preview.config.ts
@@ -1,6 +1,6 @@
 defineConfig(({ OnePasswordEnvGenerator, appNameDomainInfix }) => ({
   root: "test",
-  dockerComposePath: "test/docker-compose.yml",
+  dockerComposePath: "docker-compose.yml",
   envGenerator: OnePasswordEnvGenerator.create(
     "op://Work/n5lqo3s7ncutk4cnungs5nf27y/env"
   ),
