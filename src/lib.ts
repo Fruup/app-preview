d36476fa23d80efbb7e7a47b2091b999b00a2fdc
Leon Scherer <55341374+Fruup@users.noreply.github.com>
2025-11-15 00:29:27 +0100

    improve exec logging

--- src/lib.ts
+++ src/lib.ts
@@ -1,40 +1,89 @@
 import * as prompts from "@clack/prompts";
 import * as colors from "nanocolors";
 import { Readable } from "stream";
+import { customAlphabet } from "nanoid";
+import { eraseLines } from "ansi-escapes";
 
-const failEarly = true;
+const id = customAlphabet("abcdefghijklmnopqrstuvwxyz", 5);
 
 export async function exec(
-  cmds_: (string | boolean | number | string[] | undefined | null)[],
-  options?: Bun.SpawnOptions.OptionsObject<"pipe", "pipe", "pipe">,
-  options2: {
-    noFailEarly?: boolean;
-    onError?: (params: {
-      stderr: string;
-      preventFailEarly: () => void;
-    }) => void;
+  cmds_: (string | false | number | string[] | undefined | null)[],
+  {
+    failEarly = false,
+    expectedErrors,
+    ...spawnOptions
+  }: Bun.SpawnOptions.OptionsObject<"pipe", "pipe", "pipe"> & {
+    failEarly?: boolean;
     expectedErrors?: string[];
   } = {}
 ) {
+  const procId = id();
+
   const cmds = cmds_.flatMap(mapCmd);
-  const proc = Bun.spawn(cmds, { ...options, stdout: "pipe", stderr: "pipe" });
+  const proc = Bun.spawn(cmds, {
+    ...spawnOptions,
+    stdout: "pipe",
+    stderr: "pipe",
+  });
 
   const log = prompts.taskLog({
-    title: `Executing command: ${colors.dim(cmds.join(" "))}`,
+    title: colors.dim(cmds.join(" ")),
     input: Readable.from(proc.stdout),
   });
 
+  // Live log output
+  {
+    await Bun.$`mkdir -p ./.logs/`;
+
+    const maxLines = 20;
+    let lines: any[] = [];
+    let linesWritten = 0;
+    const stdoutReader = proc.stdout.getReader();
+
+    const stdoutLogFile = Bun.file(`./.logs/${procId}.stdout`);
+    const stdoutLogWriter = stdoutLogFile.writer();
+
+    while (true) {
+      const data = await stdoutReader.read();
+
+      if (data.value) {
+        Bun.stdout.write(
+          eraseLines(lines.length + 1 + (linesWritten > maxLines ? 1 : 0))
+        );
+
+        stdoutLogWriter.write(data.value);
+        lines.push(data.value);
+        const overflowing = lines.length > maxLines;
+
+        if (overflowing) {
+          lines = lines.slice(lines.length - maxLines);
+
+          const filename = `./.logs/${procId}.stdout`;
+
+          Bun.stdout.write(
+            `⏫︎ ${linesWritten - maxLines} more lines ⏫︎ See "${colors.dim(filename)}" for the full log.\n`
+          );
+        }
+
+        lines.forEach((line) => Bun.stdout.write(line));
+
+        linesWritten++;
+      }
+
+      if (data.done) break;
+    }
+  }
+
   await proc.exited;
 
+  // TODO: write stderr to file
+
   if (proc.exitCode === 0) {
-    log.success(`Command succeeded: ${colors.dim(cmds.join(" "))}`);
+    log.success(`✅ ${colors.dim(cmds.join(" "))}`);
   } else {
     const error = proc.stderr ? await proc.stderr.text() : undefined;
 
-    if (
-      error &&
-      options2.expectedErrors?.some((expected) => error.includes(expected))
-    ) {
+    if (error && expectedErrors?.some((expected) => error.includes(expected))) {
       log.success(
         `Command failed with expected error: ${colors.dim(cmds.join(" "))}` +
           (error ? `\n\n${error}` : "")
@@ -48,72 +97,19 @@ export async function exec(
         (error ? `\n\n${error}` : ""),
       { showLog: true }
     );
-  }
 
-  // if (
-  //   failEarly &&
-  //   !proc.success &&
-  //   !preventFailEarlyCalled &&
-  //   !options2.noFailEarly
-  // ) {
-  //   process.exit(proc.exitCode);
-  // }
+    if (failEarly) {
+      prompts.cancel("Exiting due to failure.");
+      process.exit(proc.exitCode);
+    }
+  }
 
   return proc;
 }
 
-export async function exec2(
-  cmds_: (string | boolean | number | string[] | undefined | null)[],
-  options?: Bun.SpawnOptions.OptionsObject<"pipe", "pipe", "pipe">,
-  options2: {
-    noFailEarly?: boolean;
-    onError?: (params: {
-      stderr: string;
-      preventFailEarly: () => void;
-    }) => void;
-  } = {}
-) {
-  const cmds = cmds_.flatMap(mapCmd);
-
-  console.info(`[DEBUG][CMD][STARTED]`, `${cmds.join(" ")}`);
-
-  let preventFailEarlyCalled = false;
-
-  const result = Bun.spawnSync(cmds, options);
-  if (result.success) console.log(result.stdout.toString());
-  else {
-    const stderr = result.stderr.toString();
-    console.error(`[DEBUG][CMD][ERROR]`, stderr);
-
-    options2.onError?.({
-      stderr,
-      preventFailEarly: () => {
-        preventFailEarlyCalled = true;
-      },
-    });
-  }
-
-  console.info(
-    `[DEBUG][CMD][EXITED]`,
-    `${cmds.join(" ")}`,
-    "=>",
-    result.exitCode
-  );
-
-  if (
-    failEarly &&
-    !result.success &&
-    !preventFailEarlyCalled &&
-    !options2.noFailEarly
-  ) {
-    process.exit(result.exitCode);
-  }
-
-  return result;
-}
-
 const mapCmd = (i: unknown): string[] => {
   if (typeof i === "string") return [i];
+  if (typeof i === "number") return [i.toString()];
   if (Array.isArray(i)) return i.flatMap(mapCmd);
   return [];
 };
